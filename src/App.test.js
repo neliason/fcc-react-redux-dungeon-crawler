87e01aff0b9c1a847dc8990a29028daffeab454a
Nick Eliason <nceliason@gmail.com>
2018-02-27 23:00:55 -0800

    adding tests for pickup health, pickup weapon, portal, and attack enemy

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { mount, shallow } from 'enzyme';
-import { App } from './containers/App';
 import * as Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './containers/App';
 import * as DungeonActionCreators from './actions/dungeon';
 import * as DungeonActionTypes from './actiontypes/dungeon';
 import reducer from './reducers/dungeon';
@@ -11,13 +10,11 @@ import reducer from './reducers/dungeon';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('dungeon reducer', () => {
-  let wrapper;
-  const mockMovefn = jest.fn();
-  const mockToggleLightsfn = jest.fn();
-  let mockState;
+  let mockStateBlankMap;
+  let mockStateFilledMap;
 
   beforeEach(() => {
-    mockState = {
+    mockStateBlankMap = {
       playerHealth: 100,
       weaponLevel: 0,
       playerWeapon: 'Hands',
@@ -35,6 +32,26 @@ describe('dungeon reducer', () => {
         [0, 0, 0],
         [0, 1, 0],
         [0, 0, 0],
+      ],
+    };
+    mockStateFilledMap = {
+      playerHealth: 100,
+      weaponLevel: 0,
+      playerWeapon: 'Hands',
+      playerAttack: 3,
+      playerLevel: 1,
+      playerXPToNextLevel: 20,
+      currentDungeon: 0,
+      lightsOn: true,
+      bossHealth: 400,
+      playerCoordinates: {
+        row: 1,
+        col: 1,
+      },
+      map: [
+        [0, 3, 0],
+        [2, 1, 5],
+        [0, 30, 0],
       ],
     };
   });
@@ -59,61 +76,151 @@ describe('dungeon reducer', () => {
   });
 
   it('should handle MOVE right', () => {
-    expect(reducer(mockState, {
+    expect(reducer(mockStateBlankMap, {
       type: DungeonActionTypes.MOVE,
       direction: 'Right',
     })).toEqual({
-      ...mockState,
+      ...mockStateBlankMap,
       playerCoordinates: {
         row: 1,
         col: 2,
       },
-      map: [[0, 0, 0], [0, 0, 1], [0, 0, 0]],
+      map: [
+        [0, 0, 0],
+        [0, 0, 1],
+        [0, 0, 0],
+      ],
     });
   });
 
   it('should handle MOVE left', () => {
-    expect(reducer(mockState, {
+    expect(reducer(mockStateBlankMap, {
       type: DungeonActionTypes.MOVE,
       direction: 'Left',
     })).toEqual({
-      ...mockState,
+      ...mockStateBlankMap,
       playerCoordinates: {
         row: 1,
         col: 0,
       },
-      map: [[0, 0, 0], [1, 0, 0], [0, 0, 0]],
+      map: [
+        [0, 0, 0],
+        [1, 0, 0],
+        [0, 0, 0],
+      ],
     });
   });
 
   it('should handle MOVE up', () => {
-    expect(reducer(mockState, {
+    expect(reducer(mockStateBlankMap, {
       type: DungeonActionTypes.MOVE,
       direction: 'Up',
     })).toEqual({
-      ...mockState,
+      ...mockStateBlankMap,
       playerCoordinates: {
         row: 0,
         col: 1,
       },
-      map: [[0, 1, 0], [0, 0, 0], [0, 0, 0]],
+      map: [
+        [0, 1, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ],
     });
   });
 
   it('should handle MOVE down', () => {
-    expect(reducer(mockState, {
+    expect(reducer(mockStateBlankMap, {
       type: DungeonActionTypes.MOVE,
       direction: 'Down',
     })).toEqual({
-      ...mockState,
+      ...mockStateBlankMap,
       playerCoordinates: {
         row: 2,
         col: 1,
       },
-      map: [[0, 0, 0], [0, 0, 0], [0, 1, 0]],
+      map: [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 1, 0],
+      ],
     });
   });
 
+  it('should pick up health', () => {
+    expect(reducer(mockStateFilledMap, {
+      type: DungeonActionTypes.MOVE,
+      direction: 'Left',
+    })).toEqual({
+      ...mockStateFilledMap,
+      playerCoordinates: {
+        row: 1,
+        col: 0,
+      },
+      map: [
+        [0, 3, 0],
+        [1, 0, 5],
+        [0, 30, 0],
+      ],
+      playerHealth: 120,
+    });
+  });
+
+  it('should pick up weapon', () => {
+    expect(reducer(mockStateFilledMap, {
+      type: DungeonActionTypes.MOVE,
+      direction: 'Up',
+    })).toEqual({
+      ...mockStateFilledMap,
+      playerCoordinates: {
+        row: 0,
+        col: 1,
+      },
+      map: [
+        [0, 1, 0],
+        [2, 0, 5],
+        [0, 30, 0],
+      ],
+      playerWeapon: 'Knife',
+      weaponLevel: 1,
+      playerAttack: 10,
+    });
+  });
+
+  it('should progress to next dungeon', () => {
+    expect(reducer(mockStateFilledMap, {
+      type: DungeonActionTypes.MOVE,
+      direction: 'Right',
+    })).toEqual({
+      ...mockStateFilledMap,
+      playerCoordinates: {
+        row: expect.any(Number),
+        col: expect.any(Number),
+      },
+      currentDungeon: 1,
+      map: expect.arrayContaining([expect.arrayContaining([1]), expect.arrayContaining([2]), expect.arrayContaining([3]), expect.arrayContaining([5]), expect.arrayContaining([60])]),
+    });
+  });
+
+  it('should damage enemy', () => {
+    const damageEnemyMove = reducer(mockStateFilledMap, {
+      type: DungeonActionTypes.MOVE,
+      direction: 'Down',
+    });
+    expect(damageEnemyMove.map[2][1]).toBeLessThanOrEqual(28);
+    expect(damageEnemyMove.map[2][1]).toBeGreaterThanOrEqual(26);
+    expect(damageEnemyMove.playerHealth).toBeLessThanOrEqual(91);
+    expect(damageEnemyMove.playerHealth).toBeGreaterThanOrEqual(89);
+    expect(damageEnemyMove).toEqual({
+      ...mockStateFilledMap,
+      playerHealth: expect.any(Number),
+      map: [
+        [0, 3, 0],
+        [2, 1, 5],
+        [0, expect.any(Number), 0],
+      ],
+    });
+  });
 });
 
 describe('actions', () => {
@@ -182,7 +289,7 @@ describe('App Component', () => {
   };
 
   beforeEach(() => {
-    wrapper = shallow(<App move={mockMovefn} toggleLights={mockToggleLightsfn} map={mockMap} playerHealth={mockHealth} playerWeapon={mockWeapon} playerAttack={mockAttack} playerXPToNextLevel={mockXP} currentDungeon={mockDungeon} playerLevel={mockLevel} playerCoordinates={mockCoordinates} />);
+    wrapper = Enzyme.shallow(<App move={mockMovefn} toggleLights={mockToggleLightsfn} map={mockMap} playerHealth={mockHealth} playerWeapon={mockWeapon} playerAttack={mockAttack} playerXPToNextLevel={mockXP} currentDungeon={mockDungeon} playerLevel={mockLevel} playerCoordinates={mockCoordinates} />);
   });
 
   it('renders without crashing', () => {
